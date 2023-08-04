e53de48b7c91ca674e2520062fb20308611d8b20
Lefty93 <yongchuseng@gmail.com>
2023-08-04 22:54:53 +0800

    update axiosClient

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: baseURL,
 });
 
 export default axiosClient;
